832d3d3cee50fffe5f99026e61aba745cc749087
Maja Kijewska <majkijew@student.42heilbornn.de>
2026-01-18 16:15:36 +0100

    abstract

--- ex02/Animal.hpp
+++ ex02/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 17:51:36 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/17 17:58:11 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/18 16:14:51 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ class Animal
 		virtual ~Animal();
 		void setType(std::string type);
 		std::string getType() const;
-		virtual void makeSound() const;
+		virtual void makeSound() const = 0; //pure virtual;
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 17:58:37 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/12 19:49:14 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/18 16:10:54 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,44 @@
 #include "Cat.hpp"
 #include "WrongCat.hpp"
 
-// int main()
-// {
-//     const Animal* meta = new Animal();
-//     const Animal* j = new Dog();
-//     const Animal* i = new Cat();
-//     std::cout << j->getType() << " " << std::endl;
-//     std::cout << i->getType() << " " << std::endl;
-// 	i->makeSound(); //will output the cat sound!
-//     j->makeSound();
-//     meta->makeSound();
-
-// 	const WrongAnimal* w = new WrongAnimal();
-// 	const WrongAnimal* wd = new WrongCat();
-//     // std::cout << wd->getType() << " " << std::endl;
-// 	w->makeSound();
-// 	wd->makeSound();
-
-// 	// const WrongAnimal* t = new Cat();
-	
-// }
-
 int main()
 {
-	// const Animal* a = new Animal();
-	const Animal* a = new Cat();
+    std::cout << "===== ANIMAL + BRAIN TEST =====\n";
 
-	a->makeSound();
-	return (0);
+    Dog dog;
+    Cat cat;
+
+    std::cout << "\n===== SETTING IDEAS =====\n";
+    dog.getBrain()->setIdea("I want a bone", 0);
+    dog.getBrain()->setIdea("Play fetch", 1);
+    cat.getBrain()->setIdea("I want fish", 0);
+    cat.getBrain()->setIdea("Sleep on keyboard", 1);
+
+    std::cout << "\n===== READING IDEAS =====\n";
+    std::cout << "Dog idea 0: " << dog.getBrain()->getIdea(0) << "\n";
+    std::cout << "Dog idea 1: " << dog.getBrain()->getIdea(1) << "\n";
+    std::cout << "Cat idea 0: " << cat.getBrain()->getIdea(0) << "\n";
+    std::cout << "Cat idea 1: " << cat.getBrain()->getIdea(1) << "\n";
+
+    std::cout << "\n===== POLYMORPHISM TEST =====\n";
+    Animal* animals[4];
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Dog();
+    animals[3] = new Cat();
+
+    for (int i = 0; i < 4; i++) {
+        animals[i]->makeSound();
+    }
+
+    std::cout << "\n===== COPY TEST =====\n";
+    Dog dogCopy(dog); 
+    std::cout << "DogCopy idea 0: " << dogCopy.getBrain()->getIdea(0) << "\n";
+
+    std::cout << "\n===== CLEANUP =====\n";
+    for (int i = 0; i < 4; i++)
+        delete animals[i];
+
+    std::cout << "\n===== END OF MAIN =====\n";
+    return 0;
 }
