2963c17fd25a2f3387325af1de384d80bb5d1c3d
Maja Kijewska <majkijew@student.42heilbornn.de>
2026-01-18 16:10:23 +0100

    fixed&copied cat class and .cpp from the previous ex

--- ex02/Dog.cpp
+++ ex02/Dog.cpp
@@ -6,35 +6,43 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 17:56:34 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/18 15:08:39 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/18 15:34:11 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
-Dog::Dog() : Animal() {
+Dog::Dog() {
 	_type = "Dog";
 	_brain = new Brain();
 	std::cout << "Dog Default constructor called\n";
 }
 
-Dog::Dog(const Dog &other) {
-	_brain = other._brain;
+Dog::Dog(const Dog &obj) : Animal(obj) {
+	this->_brain = new Brain(*obj._brain);
 	std::cout << "Dog copy constructor called\n";
 }
 
 Dog &Dog::operator=(const Dog &other) {
 	if (this != &other)
-		_brain = other._brain;
-	std::cout << "Dog assignment operator called\n";
+	{
+		Animal::operator=(other);
+		delete _brain;
+		this->_brain = new Brain(*other._brain);
+	}
+	std::cout << "Dog copy assigment operator called\n";
 	return (*this);
 }
 
 Dog::~Dog() {
-	delete _brain;
+	delete this->_brain;
 	std::cout << "Dog destructor\n";
 }
 
 void Dog::makeSound() const {
 	std::cout << "* Dog: woof woof *\n";
+}
+	
+Brain* Dog::getBrain() const {
+	return (this->_brain);
 }
